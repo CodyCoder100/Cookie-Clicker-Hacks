f77088a9c826bb4ec4e1e31b4f0dc6b5073d7d26
CodyCoder100 <45633957+CodyCoder100@users.noreply.github.com>
2018-12-05 12:09:33 -0700

    Paste as url in a new bookmark

---
+++ Frozen-Cookies.js
@@ -0,0 +1,9 @@
+javascript:(
+  function () {
+    var js = document.createElement('script');
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('id', 'frozenCookieScript');
+    js.setAttribute('src', 'http://icehawk78.github.io/FrozenCookies/frozen_cookies.js');
+    document.head.appendChild(js);
+ }()
+);
